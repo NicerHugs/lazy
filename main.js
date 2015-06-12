02a82903f5255b76fe9b0911efff1515b3ab04b9
Jess Scheuring <jscheuring@gmail.com>
2015-06-12 16:20:15 -0400

    adds map fn

--- main.js
+++ main.js
@@ -9,14 +9,25 @@ var Lazy = {
 
   LazyArray: function(opts) {
     this.generator = opts.generator;
+    this.queue = [];
   }
 };
 
 
 Lazy.LazyArray.prototype.take = function(n) {
+  var self = this;
   var arr = [];
   for (var i = 0; i < n; i++) {
     arr.push(this.generator());
   }
+  this.queue.forEach(function(opts) {
+    arr = arr[opts.name](opts.fn)
+  });
+  this.queue = [];
   return arr;
+};
+
+Lazy.LazyArray.prototype.map = function(callback) {
+  this.queue.push({name: 'map', fn: callback});
+  return this;
 };
